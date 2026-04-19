46cef1d3d5712da39be252e3eace3e673ddf8806
tkietctevl <tkietcte@gmail.com>
2026-04-19 22:24:20 +0700

    Node.js

---
+++ Node.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const axios = require('axios');
+const fs = require('fs');
+
+const app = express();
+app.use(express.json({ limit: '10mb' }));
+
+const TOKEN = "BOT_TOKEN";
+const CHAT_ID = "CHAT_ID";
+
+app.post('/upload', async (req, res) => {
+  const image = req.body.image.replace(/^data:image\/png;base64,/, "");
+  fs.writeFileSync("photo.png", image, 'base64');
+
+  const formData = new FormData();
+  formData.append("chat_id", CHAT_ID);
+  formData.append("photo", fs.createReadStream("photo.png"));
+
+  await axios.post(`https://api.telegram.org/bot${TOKEN}/sendPhoto`, formData, {
+    headers: formData.getHeaders()
+  });
+
+  res.send("ok");
+});
+
+app.listen(3000, () => console.log("Server chạy 3000"));
